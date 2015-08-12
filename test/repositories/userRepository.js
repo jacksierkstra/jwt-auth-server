5d6233749cd6cf1031a01968ac42bbd13249a1db
jacksierkstra <jacksierkstra@gmail.com>
2015-08-12 22:16:56 +0200

    Added boilerplate tests.

--- test/repositories/userRepository.js
+++ test/repositories/userRepository.js
@@ -2,11 +2,24 @@ var should = require('should');
 var assert = require('assert');
 var request = require('supertest');
 
-describe('User CRUD Actions', function() {
+describe('Account', function() {
 
-  it('Should return an error message when a user has already been registered.',
+  it('Should send a notification e-mail when I register as a new user.',
   function(done) {
-    done();
+    
+  done();
+  });
+
+  it('Should return an error message when a user already exists with that e-mail address.',
+  function(done) {
+
+  done();
+  });
+
+  it('Should return an error message when a user already exists but hasn\'t registered.',
+  function(done) {
+
+  done();
   });
 
 });
